71b80eeb111f936065590cad9c42a2dff2696329
Charulez <charlescorpuz440@gmail.com>
2025-09-25 02:51:15 +0800

    Add files via upload

---
+++ WD1_L2/js/script.js
@@ -0,0 +1,13 @@
+function signup() {
+  alert("Signed up successfully!");
+}
+
+function login() {
+  alert("Logged in successfully!");
+  window.location.href = "index.html";
+}
+
+function logout() {
+  alert("You have logged out successfully!");
+  window.location.href = "login.html";
+}
